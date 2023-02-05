7be9436d7e5d6e94f4f36f61201ac12d6f11d401
Timofeyyyyy <116093275+Timofeyyyyy@users.noreply.github.com>
2023-02-05 16:53:08 +0200

    finish site

--- preloader copy.js
+++
@@ -1,8 +0,0 @@
-document.body.onload = function () {
-  setTimeout(function () {
-    let preloader = document.getElementById("page-preloader");
-    if (preloader.classList.toggle("done")) {
-      preloader.classList.add("done");
-    }
-  }, 1000);
-};
